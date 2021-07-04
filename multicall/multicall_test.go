91094d3b82f1037ce5ce02298e9072f6ff16bfb7
jon4h <admin@psytux.com>
2021-07-04 23:52:47 +0200

    more tests

--- multicall/multicall_test.go
+++ multicall/multicall_test.go
@@ -47,3 +47,29 @@ func getETH(url string) (ethrpc.ETHInterface, error) {
 	provider.SetHTTPTimeout(5 * time.Second)
 	return ethrpc.New(provider)
 }
+
+func TestUnmarshaltoUint8(t *testing.T) {
+	eth, err := getETH("https://mainnet.infura.io/v3/17ed7fe26d014e5b9be7dfff5368c69d")
+	if err != nil {
+		panic(err)
+	}
+	vc := multicall.NewViewCall(
+		"key.1",
+		"0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643",
+		"decimals()(uint8)",
+		[]interface{}{},
+	)
+	vcs := multicall.ViewCalls{vc}
+	mc, _ := multicall.New(eth)
+	block := "latest"
+	res, err := mc.Call(vcs, block)
+	if err != nil {
+		panic(err)
+	}
+
+	resJson, _ := json.Marshal(res)
+	fmt.Println(string(resJson))
+	fmt.Println(res)
+	fmt.Println(res.Calls["key.1"].Decoded[0].(uint8))
+	fmt.Println(err)
+}
